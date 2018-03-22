a64b5282a67960e47c8944bc774b5553d73b30cf
pog87 <pog87@email.it>
2018-03-22 13:34:48 +0100

    poly_end

--- user_def.py
+++ user_def.py
@@ -1,25 +1,16 @@
 import sympy as s
 
-def terms_list(a,*symb):
-	# polynomial to list of monomials
-	return [s.Poly(dict([c]),*symb) for c in a.terms()]
+def terms_list(p):
+    # polynomial to list of monomials
+    return [s.Poly(dict([c]),p.free_symbols) for c in p.terms()]
 
-def exponents(p,i):
-    #lista di esponenti rispetto all'i-esima variabile
-    l=p.as_dict().items()
-    return [h[0][i] for h in l]
+def exponents(p,symb):
+    #exponent of p with respect to variable symb
+    return [s.Poly(t,symb).as_dict().items()[0][0][0] for t in terms_list(p)]
+
+def deg_monom(p):
+    #degree of each monomial in 
+    return [sum(t.as_dict().items()[0][0]) for t in terms_list(p)]
 
 def coeff(p):
-    #lista di coefficienti (ordinata come gli esponenti)
-    return [h[1] for h in p.as_dict().items()]
-
-#def gr_monom(p):
-#    return [sum(t.as_dict().items()[0][0]) for t in terms_list(a,x,y)]
-
-def gr_monom(p):
-    l=p.as_dict().items()
-    nter=len(l)
-    out=[]
-    for i in range(nter):
-        out+=[sum(l[i][0])]
-    return out
+    return [t.as_dict().items()[0][1] for t in terms_list(p)]
