10bf5368b60ec9a2c8fce513057145d0c8431950
pog87 <pog87@email.it>
2018-03-27 16:17:01 +0200

    coniche

--- user_def.py
+++ user_def.py
@@ -14,3 +14,39 @@ def deg_monom(p):
 
 def coeff(p):
     return [t.as_dict().items()[0][1] for t in terms_list(p)]
+
+
+
+def secondogrado(a,b,c):
+    if a==0:
+        return [-c/b]
+    Delta=b**2-4*a*c
+    if Delta <0:
+        return []
+    elif Delta==0:
+        return [-b/(2*a)]
+    else:
+        return [(-b-s.sqrt(Delta))/(2*a),(-b+s.sqrt(Delta))/(2*a)]
+
+def intersezione(C,r):
+    x,y=s.symbols('x y')
+    eq=C.equation(x,y)
+    c=r.coefficients
+    m,q=-c[0]/c[1], -c[2]/c[1]
+    eq2=eq.subs(y,m*x+q).as_poly().as_expr()
+    a,b,c=eq2.coeff(x**2), eq2.coeff(x), eq2.subs(x,0)
+    xlist=secondogrado(a,b,c)
+    return [s.Point(x_,m*x_+q) for x_ in xlist]
+
+def tangente(C,r):
+    x,y=s.symbols('x y')
+    eq=C.equation(x,y)
+    c=r.coefficients
+    m,q=-c[0]/c[1], -c[2]/c[1]
+    eq2=eq.subs(y,m*x+q).as_poly().as_expr()
+    a,b,c=eq2.coeff(x**2), eq2.coeff(x), eq2.subs(x,0)
+    return b**2-4*a*c==0
+
+def implicita(r):
+    c=r.coefficients
+    return -c[0]/c[1], -c[2]/c[1]
